9eb776360101dc3acab3347e45f8ff02473a4b52
Lucas <lucas.nascimento@logicsp.com.br>
2019-01-10 15:20:37 -0200

    aula3

--- loja-virtual/src/br/com/lucas/jdbc/TestaInsercao.java
+++ loja-virtual/src/br/com/lucas/jdbc/TestaInsercao.java
@@ -6,6 +6,7 @@
 package br.com.lucas.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,20 +17,28 @@ import java.sql.Statement;
  */
 public class TestaInsercao {
     public static void main(String[] args) throws SQLException {
+         String nome = "Notebook 'i3' 2013";
+         String descricao = "Notebook i3";
+        
          Connection connection = Database.getConnection();
          
-         Statement statement = connection.createStatement();
-         boolean resultado = statement.execute("insert into Produto(nome, descricao) values('Notebook', 'Asus2')",
+         String sql = "insert into Produto(nome, descricao) values(?, ?)";
+         PreparedStatement statement = connection.prepareStatement(sql, 
                  Statement.RETURN_GENERATED_KEYS);
+         statement.setString(1, "Nome");
+         statement.setString(2, descricao);
+         
+         boolean resultado = statement.execute();
+         
          System.out.println(resultado);
          
          ResultSet resultSet = statement.getGeneratedKeys();
-         
          while(resultSet.next()) {
             String id = resultSet.getString("id");
              System.out.println(id + " gerado");
          }
          
+         resultSet.close();
          statement.close();
          connection.close();
     }
